c974d931106c57c0cf91fea3ef20c31975d898f7
devfaizan <mfaizan.zahir@gmail.com>
2024-07-11 14:43:22 +0500

    city corrected, barh, layout changes, updated img

--- script.py
+++ script.py
@@ -12,12 +12,14 @@ corrections = {
     'Rawalpindi punjab': 'Rawalpindi',
     'ISB': 'Islamabad',
     'Islamabad Capital Territory': 'Islamabad',
+    ' Islamabad Capital Territory': 'Islamabad',
     'FEDERAL AREA': 'Islamabad', 
     'Federal': 'Islamabad',
     'Federal ': 'Islamabad',
     'Sheikapura': 'Sheikhupura',
     'FAISALABAD': 'Faislabad',
     'ABBOTTABAD': 'Abbottabad',
+    'Toba Tek singh': 'Toba Tek Singh',
 }
 # relpace with correct values 
 corrected_values = sauce['Domicile DISTRICT'].replace(corrections)
@@ -29,17 +31,19 @@ capitalized_values_series = pd.Series(capitalized_values)
 unique_values = capitalized_values_series.unique()
 city_counts = capitalized_values_series.value_counts()
 
-plt.figure(figsize=(10, 6))
-ax = city_counts.plot(kind='bar', color='#e3e3e3')
+plt.figure(figsize=(12, 8))
+ax = city_counts.plot(kind='barh', color='#e3e3e3')
 ax.set_facecolor('#121212')
 plt.gcf().set_facecolor('#e0e0e0')
 plt.title('Number of CS Students from Different Cities Batch 2016-2024')
-plt.xlabel('Cities')
-plt.ylabel('Number of Students')
-plt.xticks(rotation=90)
+plt.xlabel('Number of Students')
+plt.ylabel('Cities')
+# plt.xticks(rotation=90, ha='right')
+plt.subplots_adjust(bottom=0.25)
 
 for container in ax.containers:
     ax.bar_label(container, label_type='edge', color='white')
+plt.tight_layout()
 plt.show()
 print("Number of students from each city:")
 for city, count in city_counts.items():
